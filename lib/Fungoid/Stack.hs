1a7754b38aaa0bb6ad4e505740d8b8c5fd5b17db
PureFunctor <purefunctor@gmail.com>
2020-05-07 20:36:55 +0800

    Add Stack type and operations

--- lib/Fungoid/Stack.hs
+++ lib/Fungoid/Stack.hs
@@ -1,2 +1,28 @@
-module Fungoid.Stack where
+module Fungoid.Stack
+  ( Stack
+  , mkStack
+  , push
+  , pop
+  )
+where
+
+
+-- Represents a general Stack type.
+type Stack a = [a]
+
+
+-- An empty Stack for all Integral types.
+mkStack :: (Integral a) => Stack a
+mkStack = []
+
+
+-- Pushes an item to the stack.
+push :: (Integral a) => a -> Stack a -> Stack a
+push n ns = (n : ns)
+
+
+-- Pops an item from the stack.
+pop :: (Integral a) => Stack a -> (a, Stack a)
+pop []       = (0, [])
+pop (n : ns) = (n, ns)
 
