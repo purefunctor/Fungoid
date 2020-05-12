5dffcde8bf9496748c09224a4be7b1c054629618
PureFunctor <purefunctor@gmail.com>
2020-05-12 14:33:14 +0800

    Add minimal command-line tool

--- app/Fungoid.hs
+++ app/Fungoid.hs
@@ -1,8 +1,23 @@
 module Main where
 
-import qualified MyLib (someFunc)
+
+import qualified Data.ByteString as BS
+
+import Control.Monad ( void )
+
+import System.Environment ( getArgs )
+
+import Fungoid.Interpreter93
+
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  args <- getArgs
+
+  case args of
+    [file, seed] -> do
+      f <- BS.readFile file
+      void $ run f (read seed)
+    
+    _ -> putStrLn "Usage: Fungoid [file] [seed]"
+
